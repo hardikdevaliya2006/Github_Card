c2f66d2a32da432f0421048a7cc241eb8eebaf5b
hardikdevaliya2006 <hardikdevaliya2006@gmail.com>
2025-05-31 12:47:08 +0530

    fix: Set default emoji in InputDataContext to enhance user experience

--- src/Context/InputDataContext.jsx
+++ src/Context/InputDataContext.jsx
@@ -5,7 +5,7 @@ export const InputDataContext = createContext();
 export const InputDataProvider = ({ children }) => {
   const [userName, setUserName] = useState("");
   const [themeColor, setThemeColor] = useState("#28cba4");
-  const [selectedEmoji, setEmoji] = useState("");
+  const [selectedEmoji, setEmoji] = useState("👻");
 
   return (
     <InputDataContext.Provider
